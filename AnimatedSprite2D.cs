47d7e35cc52caca4eb3d2fe6235991f3fc7a08d6
Eric Hoogkamp <eric.hoog88@gmail.com>
2023-12-26 20:23:53 -0400

    Tower will rotate to follow mouse

--- AnimatedSprite2D.cs
+++ AnimatedSprite2D.cs
@@ -4,36 +4,46 @@ using System;
 public partial class AnimatedSprite2D : Godot.AnimatedSprite2D
 {
 	
-	const int SPEED = 500;
 	[Export]
 	public float RotationSpeed { get; set; } = 1.5f;
+	
+	[Export]
+	public float MovementSpeed { get; set; } = 10.5f;
 
-	private float _rotationDirection;
+	private float _rotationTarget = 45;
+	private float _rotationDirection = 0;
+	int i = 0;
 
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// Set starting location
+		Position = new Vector2(400,400);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
-	{/*
-		var vec = GetViewport().GetMousePosition() - Transform.Origin;
-		vec = SetLengthVector2(vec, delta * SPEED);
-		Transform = new Transform(vec);*/
+	{	
+		GD.Print($"Processing frame : {i}"); i++;
+		var vec = GetViewport().GetMousePosition() - Position;
+		vec = SetLengthVector2(vec);
+		GD.Print($"vec: {vec} POS: {Position}");
+		// Position
+		/*
+		vec = SetLengthVector2(vec, delta * MovementSpeed);
+		Position = Position + vec;
+		*/	
+		// Rotation
+
+		// Calculate rotation direction
+		var angle = GetAngleFromVector2(vec);
+		_rotationDirection = GetRotationDirection(angle, Rotation);
+		GD.Print($"Rotation: {Rotation} angle: {angle} _rotationDirection: {_rotationDirection}");
+		// Rotation = angle;
+		Rotation += _rotationDirection * RotationSpeed * (float)delta;
 		
-	}
-
-	public void GetInput()
-	{
-		//_rotationDirection = Input.GetAxis("left", "right");
-		//Velocity = Transform.X * Input.GetAxis("down", "up") * SPEED;
-	}
-
-	public override void _PhysicsProcess(double delta)
-	{
-		Position = Position + new Vector2(10,50);		//GetInput();
+		//GetInput();
 		//Rotation += _rotationDirection * RotationSpeed * (float)delta;
 		//MoveAndSlide();
 	}
@@ -43,14 +53,29 @@ public partial class AnimatedSprite2D : Godot.AnimatedSprite2D
 	// Extra stuff, should probably go elsewhere
 	public Vector2 SetLengthVector2(Vector2 v, double l = 1){
 		double len = Mathf.Abs(v.Length());
+		GD.Print($"len: {len} POS: {Position} l: {l}");
 		if(len == 0){
 			return v;
 		}
 		// Normalize vector and set new length
-		double multiplier = len / l;
+		double multiplier = l / len;
 		v[0] *= (float) multiplier;
 		v[1] *= (float) multiplier;
 		return v;
+	}
+
+	public int GetRotationDirection(float a1, float a2){
+		float diff = a2-a1; 
+		if(diff==0){return 0;}
+		if(Mathf.Sin(diff) < 0 ){return 1;}
+
+
+		return -1;
+
+	}
+	public float GetAngleFromVector2(Vector2 vec){
+		
+		return Mathf.Atan2(vec[0], -vec[1]);
 	}
 	
 /*
